a348c228bb41582fc9537b5331e52324c3257793
Gihozo23 <christellegihozo23@gmail.com>
2024-08-22 19:34:08 +0200

    chore: add a function to flatten the array

--- flattenArray.js
+++ flattenArray.js
@@ -1,12 +1,13 @@
 const deepNestedArray = [[[[[[[[[[[[1, 2, 3, 9]]]]]]]]]]]];
 const simpleNestedArray = [1, [2], [3, [4]], 5, [6, [7, [8], [8493, 8383]], [9, 34, [5, 6, 7]]]];
-const flattenArray = (array) => {
+
+const getNestedLevel = (array) => {
     if (!Array.isArray(array)) return 0;
     let maximumDepth = 1;
     array.forEach(item =>
     {
         if (Array.isArray(item)) {
-            const depth = flattenArray(item);
+            const depth = getNestedLevel(item);
             if (depth + 1 > maximumDepth) maximumDepth = depth + 1;
         }
     }
@@ -15,7 +16,4 @@ const flattenArray = (array) => {
     return maximumDepth;
 }
 
-console.log(flattenArray([1, [2, 3, [4]]]));
-console.log(deepNestedArray.flat(12));
-console.log(flattenArray(simpleNestedArray));
-console.log(simpleNestedArray.flat(4));
+const flattenArray = (array) => array.flat(getNestedLevel(array));

954742eab9a830639572a0c44c8195e3d841cbd4
Gihozo23 <christellegihozo23@gmail.com>
2024-08-23 14:05:07 +0200

    chore: refactor isPrime to add an early girl

--- isPrime.js
+++ isPrime.js
@@ -1,6 +1,7 @@
 const isPrime = (number) => {
+    if (number < 0 || number === 1) return false;
     for (let i = 2; i * i <= number; i++)
         if (number % i === 0)
           return false; 
-    return number > 1;
+    return true;
 }
